ffdab6525c23dd80e4996fba5f0b2fba9e07c911
Ken Lin <ken821114@example.com>
2018-07-13 01:18:22 +0800

    update myswap

--- myswap/main.c
+++ myswap/main.c
@@ -8,11 +8,20 @@ void myswap(int *a,int*b){
     *a^=*b;
 
 }
+
+void myswap2(void*a,void*b){
+    *(int*)a^=*(int*)b;
+    *(int*)b^=*(int*)a;
+    *(int*)a^=*(int*)b;
+}
 int main()
 {
     int a=20,b=10;
 
     myswap(&a,&b);
+
+    printf("a=%d b=%d\n",a,b);
+    myswap2(&a,&b);
 
     printf("a=%d b=%d\n",a,b);
 
